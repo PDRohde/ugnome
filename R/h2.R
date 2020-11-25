79453ad274e36df3d2e573ed0144700a82422f65
Palle Duun Rohde <palledr@bio.aau.dk>
2020-11-25 15:18:57 +0100

    new func

--- R/h2.R
+++ R/h2.R
@@ -1,7 +1,19 @@
+#' Convert observed heritability to the liability scale
+#'
+#' @description
+#' Whether the estiamted heritability is based on GREML or on summary statistics, if the trait is a disease trait,
+#' the estimate may be biased due to assertainment bias. To account for this, one can convert the observed heritability
+#' to the heritability on the liability scale by correcting for population disease prevalence and the proportion of cases in the sample
+#'
+#' @param h2 observed SNP heritability
+#' @param K Population disease prevalence
+#' @param P Proportion of cases in the sample
+#'
+#' @author Palle Duun Rohde
+#'
+#' @export
+#'
 obs2lia <- function(h2=NULL,K=NULL, P=NULL){
-  # h2: observed SNP heritability
-  # K: Population disease prevalence
-  # P: Proportion of cases in the sample
   X <- qnorm(K,lower.tail=FALSE)
   z <- (1/sqrt(2*pi))*(exp(-(X**2)/2))
   h2lia <- h2*(K*(1-K)*K*(1-K))/(P*(1-P)*(z**2))
